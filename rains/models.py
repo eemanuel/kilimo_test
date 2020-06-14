b279e4efd70dfa3b6582c97a0a8e5e2c8f9fdd1e
eemanuel <eemanueloviedo@gmail.com>
2020-06-14 19:09:53 -0300

    rains.models.Rain is created.

--- rains/models.py
+++ rains/models.py
@@ -1,3 +1,23 @@
-from django.db import models
+from django.db.models import CASCADE, DateTimeField, FloatField, ForeignKey
 
-# Create your models here.
+from fields.models import Field
+from utils.models import TimeStampModel
+
+
+class Rain(TimeStampModel):
+    field = ForeignKey(to=Field, on_delete=CASCADE, related_name="rain")  # muchas lluvias a un campo
+    milimeters = FloatField()
+    rain_datetime = DateTimeField()
+
+    class Meta:
+        db_table = "rains"
+
+    def __str__(self):
+        """
+        Returns a Rain's string representation.
+        """
+
+        return (
+            f"id={self.id}, land={self.field.id}, milimeters={round(self.milimeters, 3)}, "
+            f"rain_date_time={self.rain_datetime.strftime('(%Y-%m-%d, %H:%Mhs)')}"
+        )
