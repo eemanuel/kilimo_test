5f366d75449f9e36c8ad390c2e50cb067ddd6d6f
eemanuel <eemanueloviedo@gmail.com>
2020-06-15 00:41:06 -0300

    fields.urls is arranged.

--- fields/urls.py
+++ fields/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from fields import views
 
 urlpatterns = [
-    path("field/rain_avg_list/", views.FieldRainAvgListAPIView.as_view(), name="fields_rain_avg_list"),
+    path("rain_avg_list/", views.FieldRainAvgListAPIView.as_view(), name="fields_rain_avg_list"),
     path(
-        "field/accomulated_rain_list/",
+        "accomulated_rain_list/",
         views.FieldAccumulatedRainListAPIView.as_view(),
         name="fields_accomulated_rain_list",
     ),
-    path("field/<int:pk>/all_rain/", views.FieldAllRainListAPIView.as_view(), name="fields_all_rain_list"),
-    path("field/create/", views.FieldsCreateAPIView.as_view(), name="fields_create"),
+    path("<int:pk>/all_rain/", views.FieldAllRainListAPIView.as_view(), name="fields_all_rain_list"),
+    path("create/", views.FieldsCreateAPIView.as_view(), name="fields_create"),
 ]
