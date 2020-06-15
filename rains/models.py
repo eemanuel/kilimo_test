d2b3acad378c149b489a0ce40475b850949b685e
eemanuel <eemanueloviedo@gmail.com>
2020-06-14 23:44:26 -0300

    rain_datetime field change their name to date_time in rains.models.Rain .

--- rains/models.py
+++ rains/models.py
@@ -7,7 +7,7 @@ from utils.models import TimeStampModel
 class Rain(TimeStampModel):
     field = ForeignKey(to=Field, on_delete=CASCADE, related_name="rains")
     milimeters = FloatField()
-    rain_datetime = DateTimeField()
+    date_time = DateTimeField()
 
     class Meta:
         db_table = "rains"
@@ -19,5 +19,5 @@ class Rain(TimeStampModel):
 
         return (
             f"id={self.id}, land={self.field.id}, milimeters={round(self.milimeters, 3)}, "
-            f"rain_datetime={self.rain_datetime.strftime('(%Y-%m-%d, %H:%Mhs)')}"
+            f"rain_datetime={self.date_time.strftime('(%Y-%m-%d, %H:%Mhs)')}"
         )
