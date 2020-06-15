1ff6e3f90ba58636b93ef38362470a0dfd8f45ff
eemanuel <eemanueloviedo@gmail.com>
2020-06-14 22:29:56 -0300

    rains/admin is writted.

--- rains/admin.py
+++ rains/admin.py
@@ -1,3 +1,18 @@
-from django.contrib import admin
+from django.contrib.admin import ModelAdmin, site
 
-# Register your models here.
+from rains.models import Rain
+
+
+class RainAdmin(ModelAdmin):
+    model_fields_tuple = ("id", "field", "milimeters", "rain_datetime")
+
+    # set presentational fields
+    list_display = model_fields_tuple
+    search_fields = model_fields_tuple
+    ordering = ("id",)
+
+    # set detail fields
+    fields = ["field", "milimeters", "rain_datetime"]
+
+
+site.register(Rain, RainAdmin)
