784574ad8bf36bece02abca4d4145e082b3dffcd
eemanuel <eemanueloviedo@gmail.com>
2020-06-15 00:41:23 -0300

    rains.urls is arranged.

--- rains/urls.py
+++ rains/urls.py
@@ -1,4 +1,4 @@
 from django.urls import path
 from rains import views
 
-urlpatterns = [path("rain/create/", views.RainCreateAPIView.as_view(), name="rain_create")]
+urlpatterns = [path("create/", views.RainCreateAPIView.as_view(), name="rain_create")]
