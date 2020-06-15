a75106b424858651318e2261be9abfab3764ae58
eemanuel <eemanueloviedo@gmail.com>
2020-06-14 22:29:43 -0300

    fields/admin is writted.

--- fields/admin.py
+++ fields/admin.py
@@ -1,3 +1,30 @@
-from django.contrib import admin
+from django.contrib.admin import ModelAdmin, site, TabularInline
 
-# Register your models here.
+from fields.models import Field
+from rains.models import Rain
+
+
+class RainInline(TabularInline):
+    model = Rain
+    fields = ("id",)
+
+
+class FieldAdmin(ModelAdmin):
+
+    # set presentational fields
+    list_display = ("id", "owner", "name", "hectares")
+    search_fields = ("id", "name",)
+    ordering = ("id",)
+
+    # set detail fields
+    inlines = [RainInline]  # only with a model in foreignkey
+    fields = [
+        "owner",
+        "name",
+        "hectares",
+        "latitude",
+        "longitude",
+    ]
+
+
+site.register(Field, FieldAdmin)
